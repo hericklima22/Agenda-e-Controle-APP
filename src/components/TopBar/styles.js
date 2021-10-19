b9d7f59242a488c6bba5bad8cb1e9a5b58f27543
hericklima22 <herick.lima77@gmail.com>
2021-10-19 17:15:31 -0300

    feat: top bar component

---
+++ src/components/TopBar/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 0,
+    paddingBottom: 13,
+    paddingTop: 13,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    backgroundColor: "yellow",
+  },
+  title: { fontSize: 50 },
+});
+
+export default styles;
