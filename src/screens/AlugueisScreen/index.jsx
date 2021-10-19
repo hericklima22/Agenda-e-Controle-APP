c6b60008698571505a82937eb50e9d6059846826
hericklima22 <herick.lima77@gmail.com>
2021-10-19 17:16:57 -0300

    feat: alugueis screen init

---
+++ src/screens/AlugueisScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TopBar } from "../../components/TopBar";
+import styles from "./styles";
+
+export function AlugueisScreen() {
+  return (
+    <SafeAreaView>
+      <TopBar>Teste</TopBar>
+    </SafeAreaView>
+  );
+}
---
+++ src/screens/index.js
@@ -0,0 +1,1 @@
+export { AlugueisScreen } from "./AlugueisScreen";
