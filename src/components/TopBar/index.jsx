276d56bee31f1ef62b273106c4ba804d65b9ef4d
hericklima22 <herick.lima77@gmail.com>
2021-10-26 23:01:58 -0300

    feat: topbar component

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import styles from "./styles";
 
 export function TopBar(props) {
