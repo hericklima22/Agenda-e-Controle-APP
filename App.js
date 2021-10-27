d93b782ad7bfee4ce3e27d19ecf556aa31eec5e2
hericklima22 <herick.lima77@gmail.com>
2021-10-27 00:12:37 -0300

    fir: meking some changes

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { AlugueisScreen } from "./src/screens/AlugueisScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { BottomNavigation } from "./src/navigation";
 
 export default function App() {
-  return <AlugueisScreen />;
+  return (
+    <NavigationContainer>
+      <BottomNavigation />
+    </NavigationContainer>
+  );
 }
--- src/screens/AlugueisScreen/styles.js
+++ src/screens/AlugueisScreen/styles.js
@@ -4,6 +4,10 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 50,
   },
+  containerImage: {
+    width: "50%",
+    marginLeft: "1.5%",
+  },
 });
 
 export default styles;
