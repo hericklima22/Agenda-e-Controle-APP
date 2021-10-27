4f1e822d866fff5688f331481f116fd8d570f7de
hericklima22 <herick.lima77@gmail.com>
2021-10-27 10:02:08 -0300

    feat: flat list implemented

--- src/screens/AlugueisScreen/index.jsx
+++ src/screens/AlugueisScreen/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TopBar } from "../../components/TopBar";
 import Equipamentos from "./equipamentos.json";
@@ -7,18 +7,39 @@ import styles from "./styles";
 
 export function AlugueisScreen() {
   const { equipamentos } = Equipamentos;
+
   return (
     <SafeAreaView>
       <TopBar>Alugueis</TopBar>
-      <FlatList
-        keyExtractor={equipamentos.id}
-        data={equipamentos}
-        renderItem={({ item, index }) => (
-          <View key={item.id} style={styles.containerImage}>
-            <Text>{item.nome}</Text>
-          </View>
-        )}
-      ></FlatList>
+      <View style={{ height: 2, backgroundColor: "#000000" }}></View>
+      <View>
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={equipamentos}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              style={
+                (styles.list,
+                item.id % 2 === 0
+                  ? { backgroundColor: "#ffdb58" }
+                  : { backgroundColor: "#ecec52" })
+              }
+            >
+              <View style={styles.containerView}>
+                <View style={styles.itemView1}>
+                  <Text style={styles.itemText1}>{item.nome_cliente}</Text>
+                  <Text style={styles.itemText2}>{item.tipo}</Text>
+                </View>
+                <View>
+                  <Text style={styles.itemText3}>
+                    Vence: {item.data_vencimento}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        ></FlatList>
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/AlugueisScreen/styles.js
+++ src/screens/AlugueisScreen/styles.js
@@ -4,9 +4,25 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 50,
   },
-  containerImage: {
-    width: "50%",
-    marginLeft: "1.5%",
+  list: {},
+  containerView: {
+    display: "flex",
+    flexDirection: "row",
+    margin: "2%",
+    borderColor: "#000000",
+  },
+  itemView1: {
+    width: "55%",
+    marginLeft: "2.5%",
+  },
+  itemText1: {
+    fontSize: 25,
+  },
+  itemText2: {
+    fontSize: 18,
+  },
+  itemText3: {
+    fontSize: 18,
   },
 });
 
